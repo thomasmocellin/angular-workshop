34efcf9226aa7d9e614dee74f55d7673d452ad07
ThomasMocellin <76048214+thomasmocellin@users.noreply.github.com>
2025-09-16 13:49:25 +0200

    remove credentials from code

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD-7jn7sMHsN9MGH51LhJLTxCLCDEMx7mI',
+    authDomain: 'angular-workshop-95795.firebaseapp.com',
+    databaseURL: 'https://angular-workshop-95795-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'angular-workshop-95795',
+    storageBucket: 'angular-workshop-95795.firebasestorage.app',
+    messagingSenderId: '92874982361',
+    appId: '1:92874982361:web:6350cc9f5c2da5eaf38fc8',
+    measurementId: 'G-YN9PPMP87R',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,12 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
 
 import { AppComponent } from './app/app';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyD-7jn7sMHsN9MGH51LhJLTxCLCDEMx7mI',
-  authDomain: 'angular-workshop-95795.firebaseapp.com',
-  databaseURL: 'https://angular-workshop-95795-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'angular-workshop-95795',
-  storageBucket: 'angular-workshop-95795.firebasestorage.app',
-  messagingSenderId: '92874982361',
-  appId: '1:92874982361:web:6350cc9f5c2da5eaf38fc8',
-  measurementId: 'G-YN9PPMP87R',
-};
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter([]),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideDatabase(() => getDatabase()),
   ],
 }).catch((err) => console.error(err));
